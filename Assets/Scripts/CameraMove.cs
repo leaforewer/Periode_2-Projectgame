67d8b5970c930eb4bc0483e6a32a5ebde0ab9db7
Ahmet Erdogan <329188@student.mboutrecht.nl>
2019-12-06 17:42:26 +0100

    asdsad

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+ using System.Collections;
+ 
+ public class CameraMove : MonoBehaviour {
+ 
+ 
+     public void PlayAnimation() {
+         GetComponent<Animator>().SetBool("Throw",true);
+     }
+ 
+ }
