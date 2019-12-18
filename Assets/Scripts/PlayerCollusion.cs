281a70d6fdc2e39e3d1b246270db1df555d81291
Ahmet Erdogan <329188@student.mboutrecht.nl>
2019-12-18 13:04:16 +0100

    Maze start

---
+++ Assets/Scripts/PlayerCollusion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollusion : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+    public GameObject gameManager;
+
+    
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().Endgame();
+
+        }
+    }
+}
