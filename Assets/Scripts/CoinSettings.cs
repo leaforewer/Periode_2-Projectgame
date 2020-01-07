39ea5305ce29a0f2f3933b1a6eeca5c171b9441b
Ahmet Erdogan <329188@student.mboutrecht.nl>
2020-01-07 16:07:02 +0100

    selamun aleykum

---
+++ Assets/Scripts/CoinSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSettings : MonoBehaviour
+{
+    public float turnspeed = 100;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * turnspeed * Time.deltaTime);
+    }
+}
